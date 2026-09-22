988d3203e8e1634e4b7ccaad4bd618a4ab1324c2
raymond-nault <raymond.nault001@albright.edu>
2026-09-22 10:50:07 -0400

    new commit

---
+++ 04_working_with_lists/04_04_one_million.py
@@ -0,0 +1,12 @@
+'''
+
+Raymond Nault
+Chapter 4: Working with Lists
+
+'''
+
+# this loop will print the numbers 1 through 1,000,000
+
+numbers = list(range(1, 1000001))
+for number in (range(1, 1000001)):
+    print(number)
